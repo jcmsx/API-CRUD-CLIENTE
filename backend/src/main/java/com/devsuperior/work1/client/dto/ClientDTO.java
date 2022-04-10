cebf03f11399c2ce4716e7d83114f615cd67206c
Juliano Carlos de Oliveira <jcmsx@yahoo.com.br>
2022-04-10 16:04:43 -0300

    implements classes Client and ClientDTO

--- backend/src/main/java/com/devsuperior/work1/client/dto/ClientDTO.java
+++ backend/src/main/java/com/devsuperior/work1/client/dto/ClientDTO.java
@@ -1,5 +1,89 @@
 package com.devsuperior.work1.client.dto;
 
-public class ClientDTO {
+import java.io.Serializable;
+import java.time.Instant;
 
+import com.devsuperior.work1.client.entities.Client;
+
+public class ClientDTO 	implements Serializable {
+		private static final long serialVersionUID = 1L;
+
+		private Long id;
+		private String name;
+		private String cpf;
+		private Double income;
+		private Instant birtDate;
+		private Integer children;
+
+		public ClientDTO() {
+
+		}
+
+		public ClientDTO(Long id, String name, String cpf, Double income, Instant birtDate, Integer children) {
+			this.id = id;
+			this.name = name;
+			this.cpf = cpf;
+			this.income = income;
+			this.birtDate = birtDate;
+			this.children = children;
+		}
+
+		public ClientDTO(Client entity) {
+			this.id = entity.getId();
+			this.name = entity.getName();
+			this.cpf = entity.getCpf();
+			this.income = entity.getIncome();
+			this.birtDate = entity.getBirtDate();
+			this.children = entity.getChildren();
+		}
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getCpf() {
+			return cpf;
+		}
+
+		public void setCpf(String cpf) {
+			this.cpf = cpf;
+		}
+
+		public Double getIncome() {
+			return income;
+		}
+
+		public void setIncome(Double income) {
+			this.income = income;
+		}
+
+		public Instant getBirtDate() {
+			return birtDate;
+		}
+
+		public void setBirtDate(Instant birtDate) {
+			this.birtDate = birtDate;
+		}
+
+		public Integer getChildren() {
+			return children;
+		}
+
+		public void setChildren(Integer children) {
+			this.children = children;
+		}
+		
+		
 }
