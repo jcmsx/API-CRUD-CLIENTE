95d94d72e0c3edcc0091a314307da1a125ce9c5f
Juliano Carlos de Oliveira <jcmsx@yahoo.com.br>
2022-04-10 16:14:20 -0300

    implements classes ClientRepository

--- backend/src/main/java/com/devsuperior/work1/client/repositories/ClientRepository.java
+++ backend/src/main/java/com/devsuperior/work1/client/repositories/ClientRepository.java
@@ -1,5 +1,11 @@
 package com.devsuperior.work1.client.repositories;
 
-public class ClientRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.devsuperior.work1.client.entities.Client;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
 
 }
